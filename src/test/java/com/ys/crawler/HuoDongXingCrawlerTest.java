95d5cb599abc5da4876f32b0cdbdf5150c4a01c7
brook <Brookchai@bangbangzhang.com>
2015-11-26 11:04:52 +0800

    modify

--- src/test/java/com/ys/crawler/HuoDongXingCrawlerTest.java
+++ src/test/java/com/ys/crawler/HuoDongXingCrawlerTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)  
 @ContextConfiguration(locations = {"classpath:dispatcher-servlet.xml"}) 
-public class SimpleClientTest {
+public class HuoDongXingCrawlerTest {
 	
 	@Autowired
 	HuoDongXingCrawler sc;
